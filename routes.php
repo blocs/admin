db1e6b701c6a03726a85076c6df2be60fe93f672
Hiroyuki Yada <yada@linear-jp.biz>
2022-10-20 21:15:23 +0900

    Maintenanceに確認画面を追加

--- routes.php
+++ routes.php
@@ -29,8 +29,12 @@ Route::middleware('guest')
 		Route::get('/', [UserController::class, 'index'])->name('index');
 		Route::post('/', [UserController::class, 'index'])->name('search');
 		Route::get('/entry/{id?}', [UserController::class, 'entry'])->name('entry');
+		Route::post('/entry/{id?}', [UserController::class, 'entry'])->name('back');
+		Route::post('/confirm_insert', [UserController::class, 'confirm_insert'])->name('confirm_insert');
 		Route::post('/insert', [UserController::class, 'insert'])->name('insert');
+		Route::post('/confirm_update/{id}', [UserController::class, 'confirm_update'])->name('confirm_update');
 		Route::post('/update/{id}', [UserController::class, 'update'])->name('update');
+		Route::post('/confirm_delete/{id?}', [UserController::class, 'confirm_delete'])->name('confirm_delete');
 		Route::post('/delete/{id?}', [UserController::class, 'delete'])->name('delete');
 		Route::post('/toggle/{id}', [UserController::class, 'toggle'])->name('toggle');
 	}
