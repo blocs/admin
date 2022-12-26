72204137785c698b312ea947f0dd65f5b31cc2e5
Hiroyuki Yada <yada@linear-jp.biz>
2022-12-26 22:35:14 +0900

    Refactor

--- stubs/app/Http/Controllers/Admin/HomeController.php
+++ stubs/app/Http/Controllers/Admin/HomeController.php
@@ -55,6 +55,7 @@ class HomeController extends Controller
 
         $current = User::count();
         $param['current'] = $current;
+        $param['update'] = date(DATE_ATOM);
 
         $insertList = [];
         $recordList = User::where(
@@ -82,41 +83,33 @@ class HomeController extends Controller
             ++$deleteList[$month];
         }
 
-        $each1 = [];
-        $each2 = [];
-        $accumulate = [];
+        $line1 = [];
+        $bar1 = [];
+        $bar2 = [];
         $yaxisMax = 0;
         $y2axisMax = 0;
 
-        $bar1 = [];
-        $bar2 = [];
-        $line1 = [];
-
         foreach ($xaxis as $xlabel) {
-            array_unshift($accumulate, [$xlabel, $current]);
             $line1[] = $current;
             $y2axisMax < $current && $y2axisMax = $current;
 
             $each = isset($insertList[$xlabel]) ? $insertList[$xlabel] : 0;
-            array_unshift($each1, [$xlabel, $each]);
-            $bar1[] = $each;
-            $current -= $each;
             $yaxisMax < $each && $yaxisMax = $each;
 
+            $bar1[] = $each;
+            $current -= $each;
+
             $each = isset($deleteList[$xlabel]) ? $deleteList[$xlabel] : 0;
-            array_unshift($each2, [$xlabel, $each]);
+            $yaxisMax < $each && $yaxisMax = $each;
+
             $bar2[] = $each;
             $current += $each;
-            $yaxisMax < $each && $yaxisMax = $each;
         }
 
         $param['graphLabels'] = json_encode(array_reverse($xaxis));
         $param['graphDataBar1'] = json_encode(array_reverse($bar1));
         $param['graphDataBar2'] = json_encode(array_reverse($bar2));
         $param['graphDataLine1'] = json_encode(array_reverse($line1));
-
-        $param['graphJson'] = json_encode([$each1, $each2, $accumulate]);
-        $param['update'] = date(DATE_ATOM);
 
         $param['graphYaxisMax'] = self::getYaxisMax($yaxisMax);
         $param['graphY2axisMax'] = self::getYaxisMax($y2axisMax);
