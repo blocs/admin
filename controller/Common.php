a27b1e0f3409c16400f7cc087fca9993651c4c3e
Hiroyuki Yada <yada@linear-jp.biz>
2023-01-09 23:24:10 +0900

    terminate 不要なメソッドを廃止

--- controller/Common.php
+++ controller/Common.php
@@ -37,33 +37,4 @@ trait Common
             $this->val[$keyItem] = session($sessionKey);
         }
     }
-
-    protected function addItem(&$valTable, $itemName, $tableName, $key, $value)
-    {
-        $keyList = [];
-        foreach ($valTable as $num => $buff) {
-            empty($buff[$itemName]) || $keyList[] = $buff[$itemName];
-        }
-        $keyList = array_merge(array_unique($keyList));
-
-        // テーブルより追加データを取得
-        if (class_exists($tableName)) {
-            // Eloquent
-            $table = call_user_func($tableName.'::select', $key, $value);
-            $tableDataList = $table->whereIn($key, $keyList)->get();
-        } else {
-            // クエリビルダ
-            $tableDataList = \DB::table($tableName)->select($key, $value)->whereIn($key, $keyList)->get();
-        }
-
-        $replaceItem = [];
-        foreach ($tableDataList as $tableData) {
-            $replaceItem[$tableData->$key] = $tableData->$value;
-        }
-
-        // 元データに追加
-        foreach ($valTable as $num => $buff) {
-            $valTable[$num][$value] = empty($replaceItem[$buff[$itemName]]) ? '' : $replaceItem[$buff[$itemName]];
-        }
-    }
 }
