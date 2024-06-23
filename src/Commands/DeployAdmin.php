8c9a2a76d1f34e2f08cbeb55ef027cd55dc32536
Hiroyuki Yada <yada@linear-jp.biz>
2024-06-23 15:23:31 +0900

    コマンドの場所を変更

--- src/Commands/DeployAdmin.php
+++ src/Commands/DeployAdmin.php
@@ -36,10 +36,10 @@ class DeployAdmin extends Deploy
         \Artisan::call('migrate');
         \Artisan::call('db:seed', ['--class' => 'AdminSeeder']);
 
-        echo "Deploy was completed successfully.\n";
+        \Artisan::call('route:cache');
 
+        echo "Deploy was completed successfully.\n";
         echo 'Login URL is '.route('login').".\n";
         echo "Initial ID/Pass is admin/admin.\n";
-        \Artisan::call('route:clear');
     }
 }
