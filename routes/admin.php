8994f9241673ae581cd74f575d32aaf5261508bd
Hiroyuki Yada <yada@linear-jp.biz>
2024-06-23 15:01:43 +0900

    コントローラーの場所を変更

--- routes/admin.php
+++ routes/admin.php
@@ -1,9 +1,9 @@
 <?php
 
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\Admin\HomeController;
+use App\Http\Controllers\Admin\LoginController;
+use App\Http\Controllers\Admin\ProfileController;
+use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['web'])
