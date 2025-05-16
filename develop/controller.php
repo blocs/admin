04d0d7f05c827998859478e80682bbd51894dd8b
Hiroyuki Yada <yada@linear-jp.biz>
2025-05-17 01:08:07 +0900

    add Devin用のドキュメントを作成
    JSONファイルの作り方

--- develop/controller.php
+++ develop/controller.php
@@ -12,4 +12,37 @@ class CONTROLLER_BASENAME extends \Blocs\Controllers\Base
         $this->paginateNum = PAGINATE_NUM;
         $this->noticeItem = 'NOTICE_ITEM';
     }
+
+    protected function prepareIndexSearch(&$mainTable)
+    {
+        // 検索
+        foreach ($this->searchItems as $searchItem) {
+            $mainTable->where(function ($query) use ($searchItem) {
+                $query
+                    ->where('item1', 'LIKE', '%'.$searchItem.'%')
+                    ->orWhere('item2', 'LIKE', '%'.$searchItem.'%');
+            });
+        }
+        docs([
+            '<search>があれば、<'.$this->loopItem.'>のitem1を<search>で部分一致検索',
+            '<search>があれば、<'.$this->loopItem.'>のitem2を<search>で部分一致検索',
+        ]);
+
+        // ソート
+        $this->keepItem('sort');
+
+        // デフォルトのソート条件
+        if (empty($this->val['sort']) || !is_array($this->val['sort'])) {
+            $this->val['sort'] = [];
+        } else {
+            $this->val['sort'] = array_filter($this->val['sort'], 'strlen');
+        }
+        count($this->val['sort']) || $this->val['sort'] = ['item1' => 'asc'];
+
+        // 指定された条件でソート
+        foreach (['item1', 'item2', 'item3'] as $sortItem) {
+            empty($this->val['sort'][$sortItem]) || $mainTable->orderBy($sortItem, $this->val['sort'][$sortItem]);
+        }
+        docs('指定された条件でソート');
+    }
 }
